c2642e7c6b3157e2469cef2bf57d310108b26466
Matt Dziuban <mrdziuban@gmail.com>
2014-04-14 23:00:19 -0400

    Add string reversal.

---
+++ my_solutions/Text/reverse_str.py
@@ -0,0 +1,13 @@
+def reverse(string):
+  last_idx = len(string) - 1
+  res = ''
+  while last_idx >= 0:
+    res += string[last_idx]
+    last_idx -= 1
+  return res
+
+def reverse_easy(string):
+  return string[::-1]
+
+string = raw_input('Enter string: ')
+print reverse_easy(string)
